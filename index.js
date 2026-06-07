47816fba7c62cb5755b33236b4b7ee59049434d5
DoanBackendn <ddoan04030l@gmail.com>
2026-06-07 22:30:51 +0700

    fix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
 }))
 app.use(express.json())
 
-mongoose.connect("process.env.MONGODB_URI")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Kết nối MongoDB thành công!"))
   .catch((err) => console.log("Lỗi kết nối:", err))
 
